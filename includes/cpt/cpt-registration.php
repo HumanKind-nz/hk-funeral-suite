7ceb9596c021969d0ffe8d64612459d4a5e8d3af
Gareth Bissland <5170216+gbissland@users.noreply.github.com>
2025-03-23 16:58:58 +1300

    Update cpt-registration.php

--- includes/cpt/cpt-registration.php
+++ includes/cpt/cpt-registration.php
@@ -4,9 +4,10 @@
  *
  * @package    HK_Funeral_Suite
  * @subpackage CPT
- * @version    1.1.0
+ * @version    1.1.1
  * @since      1.3.0
  * @changelog
+ *   1.1.1 - Monuments cpt added
  *   1.1.0 - Updated to use hook system for dynamic CPT registration
  *   1.0.2 - Initial factory implementation
  */
@@ -51,8 +52,6 @@ function hk_fs_register_product_cpts() {
         ]);
     }
     
-    // Example of a new CPT - uncomment to use
-    /*
     if ($settings->is_cpt_enabled('monuments')) {
         HK_Funeral_Product_CPT_Factory::register_product_cpt([
             'post_type' => 'monument', 
@@ -62,7 +61,8 @@ function hk_fs_register_product_cpts() {
             'svg_icon' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="%23a7aaad" d="M240 0C266.5 0 288 21.49 288 48V96H224V48C224 21.49 245.5 0 272 0zM192 352V480C192 497.7 177.7 512 160 512C142.3 512 128 497.7 128 480V352H192zM320 480C320 497.7 305.7 512 288 512C270.3 512 256 497.7 256 480V352H320V480zM80 32C106.5 32 128 53.49 128 80V96H32V80C32 53.49 53.49 32 80 32zM32 128H128V224H32V128zM32 256H128V320H32V256zM320 128H416V224H320V128zM320 256H416V320H320V256zM384 32C410.5 32 432 53.49 432 80V96H336V80C336 53.49 357.5 32 384 32zM464 32C490.5 32 512 53.49 512 80V160C512 213 469 256 416 256H448C465.7 256 480 270.3 480 288C480 305.7 465.7 320 448 320H64C46.33 320 32 305.7 32 288C32 270.3 46.33 256 64 256H96C42.98 256 0 213 0 160V80C0 53.49 21.49 32 48 32H464zM224 128V224H160V128H224zM256 128H288V224H256V128z"/></svg>'
         ]);
     }
-    
+    // Example of a new CPT - uncomment to use
+    /*
     if ($settings->is_cpt_enabled('keepsakes')) {
         HK_Funeral_Product_CPT_Factory::register_product_cpt([
             'post_type' => 'keepsake', 
